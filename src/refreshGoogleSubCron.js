a25a8ed798d09ee70ef4a44b69e6fb416888165c
da kong <kongda9999@gmail.com>
2022-05-30 18:30:45 +0800

    trying to fix google sub cron issue

--- src/refreshGoogleSubCron.js
+++ src/refreshGoogleSubCron.js
@@ -5,25 +5,24 @@ const subscriptionHandler = require('./server/handlers/subscriptionHandler');
 // Google Drive notification subscription expiry is default to 24 hours.
 // This cron job is to refresh all Google Drive subscriptions every 12 hours.
 async function refreshSubscription() {
-    try {
-        let successMessage = 'Google Drive Notification Subscription\n';
-        const googleUsers = await GoogleUser.findAll();
-        for (const googleUser of googleUsers) {
+    let successMessage = 'Google Drive Notification Subscription\n';
+    const googleUsers = await GoogleUser.findAll();
+    for (const googleUser of googleUsers) {
+        try {
             console.log(`refreshing subscriptions for user: ${googleUser.email}...`);
             await subscriptionHandler.refreshSubscriptionForUser(googleUser);
         }
-        console.log(successMessage);
-        return;
+        catch (e) {
+            console.log(e.message);
+            await axios.post(
+                'https://hooks.ringcentral.com/webhook/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvdCI6InUiLCJvaSI6IjE0MDU1MDk2NDgzODciLCJpZCI6IjEzNzg3NTQ1ODcifQ.Nq6z0NWegffNJWZdPIOjePFfCUCgK3bBCk4Z3SDY_hY',
+                {
+                    "title": `refresh subscription error for ${googleUser.email}\nError: ${e.message}`
+                }
+            );
+        }
     }
-    catch (e) {
-        console.log(e.message);
-        axios.post(
-            'https://hooks.ringcentral.com/webhook/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvdCI6InUiLCJvaSI6IjE0MDU1MDk2NDgzODciLCJpZCI6IjEzNzg3NTQ1ODcifQ.Nq6z0NWegffNJWZdPIOjePFfCUCgK3bBCk4Z3SDY_hY',
-            {
-                "title": `refresh subscription error: ${e.message}`
-            }
-        );
-    }
+    console.log(successMessage);
 }
 
 // Commented out. It's for local testing
