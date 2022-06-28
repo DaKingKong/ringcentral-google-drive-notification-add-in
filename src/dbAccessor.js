351025a1c07c5462a93aff0485b65566bc9a5c36
da kong <kongda9999@gmail.com>
2022-06-28 13:54:00 +0800

    added dbAccessor

--- src/dbAccessor.js
+++ src/dbAccessor.js
@@ -1,11 +1,20 @@
-// require('dotenv').config();
+require('dotenv').config();
 const { sequelize } = require('./models/sequelize');
+const { default: Bot } = require('ringcentral-chatbot-core/dist/models/Bot');
 
 async function executeQuery(input) {
     try {
-        console.log(input.dbQuery);
-        const result = await sequelize.query(input.dbQuery);
-        console.log(JSON.stringify(result, null, 2));
+        if (input.Query) {
+            console.log(input.dbQuery);
+            const result = await sequelize.query(input.dbQuery);
+            console.log(JSON.stringify(result, null, 2));
+        }
+        if (input.ensureWebhook) {
+            const bots = await Bot.findAll();
+            for (const bot of bots) {
+                await bot.ensureWebHook();
+            }
+        }
     }
     catch (e) {
         console.error(e.message);
